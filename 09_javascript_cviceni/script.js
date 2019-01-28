c54eac008ba365d42f5aed4ad0a726f8e09c2e47
Pavel Husa <pavel.husa@gmail.com>
2019-01-28 16:15:27 +0100

    JavaScript 28.1.

---
+++ 09_javascript_cviceni/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('generate');
+    console.log(button);
+    button.addEventListener('click', generate);
+});
+
+const generate = () => {
+    const name = document.getElementById('name').value;
+    const type = document.querySelectorAll('input[name="tp"]:checked')[0];
+    const out = document.getElementById('outputWrapper');
+    //console.log(name.length);
+    if (name === '') {
+        out.innerText = 'Vypln jmeno!!!';
+        return;
+    }
+    if (type.value === 'greet') {
+        out.innerText = 'Ahoj ' + name + ', přeji hezký den!!';
+    } else {
+        out.innerText = name.value + ' jsi nejlepsi!!!';
+    }
+
+}
